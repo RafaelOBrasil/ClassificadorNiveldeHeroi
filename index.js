431f1a7b7d8c3e41d0914493fb9bc8b1fdec4bd6
RafaelOBrasil <rafael-lunia@hotmail.com>
2024-10-02 13:18:29 -0300

    Att Output

--- index.js
+++ index.js
@@ -5,29 +5,30 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Informe o valor total do XP para verificar o nivel: ', (XP) => {
-    if (XP <= 1000) {
-        console.log("Ferro")
-    } else if(XP <= 2000) {
-        console.log("Bronze")
-    }else if(XP <= 5000) {
-        console.log("Prata")
-    }else if(XP <= 7000) {
-        console.log("Ouro")
-    }else if(XP <= 8000) {
-        console.log("Platina")
-    }else if(XP <= 9000) {
-        console.log("Ascendente")
-    }else if(XP <= 10000) {
-        console.log("Imortal")
-    }else{
-        console.log("Radiante")
-    }
 
-
-    rl.close()
+rl.question("Nome do Heroi: ", (nome) => {
+    rl.question('Informe o valor total do XP para verificar o nivel: ', (XP) => {
+        if (XP <= 1000) {
+            console.log("O Herói de nome " + nome +" está no nível de Ferro")
+        }else if(XP <= 2000) {
+            console.log("O Herói de nome " + nome +" está no nível de Bronze")
+        }else if(XP <= 5000) {
+            console.log("O Herói de nome " + nome +" está no nível de Prata")
+        }else if(XP <= 7000) {
+            console.log("O Herói de nome " + nome +" está no nível de Ouro")
+        }else if(XP <= 8000) {
+            console.log("O Herói de nome " + nome +" está no nível de Platina")
+        }else if(XP <= 9000) {
+            console.log("O Herói de nome " + nome +" está no nível de Ascendente")
+        }else if(XP <= 10000) {
+            console.log("O Herói de nome " + nome +" está no nível de Imortal")
+        }else{
+            console.log("O Herói de nome " + nome +" está no nível de Radiante")
+        }
+    
+    
+        rl.close()
+    });
 });
-
-
 
 
